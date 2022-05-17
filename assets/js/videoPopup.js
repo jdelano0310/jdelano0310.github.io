14ffc21ee7218bc96d20f84d4d25e905b57a1ff7
Jon Delano <jd31068@hotmail.com>
2022-05-17 09:33:22 -0400

    modify grt code as Twitch and FB vids were added

--- assets/js/videoPopup.js
+++ assets/js/videoPopup.js
@@ -1,20 +1,22 @@
 /*!
+ * Based on **************
  * GRT Youtube Popup - jQuery Plugin
  * Version: 1.0
  * Author: GRT107
  *
  * Copyright (c) 2017 GRT107
  * Released under the MIT license
+ * modified to allow it to display Twitch clips and videos from Facebook - 2022 JDelano0310
 */
 
 (function ( $ ) {
 
-	$.fn.grtyoutube = function( options ) {
+	$.fn.videoPopup = function( options ) {
 
 		return this.each(function() {
 
 			// Get video ID
-			var getvideoid = $(this).attr("youtubeid");
+			var getvideoid = $(this).attr("videoid");
 			var videoPlayback = '';
 			var src = '';
 			var popupIfame = '';
@@ -28,18 +30,21 @@
 
 			// Convert some values
 			if(settings.autoPlay === true) { settings.autoPlay = 1 } else if(settings.autoPlay === false)  { settings.autoPlay = 0 }
-			if(settings.theme === "dark") { settings.theme = "grtyoutube-dark-theme" } else if(settings.theme === "light")  { settings.theme = "grtyoutube-light-theme" }
+			if(settings.theme === "dark") { settings.theme = "videoPopup-dark-theme" } else if(settings.theme === "light")  { settings.theme = "videoPopup-light-theme" }
 			
 			if(settings.videoID.indexOf('twitch') > -1) {
+				// twitch video request
 				src = settings.videoID.split('/')[1];
-				popupIfame='<iframe class="grtyoutube-iframe" src="https://clips.twitch.tv/embed?clip=' + src + '&parent=www.fansofstapes.com" autoplay="true" frameborder="0" allowfullscreen="false" scrolling="no"></iframe>';
+				popupIfame='<iframe class="videoPopup-iframe" src="https://clips.twitch.tv/embed?clip=' + src + '&parent=www.fansofstapes.com" autoplay="true" frameborder="0" allowfullscreen="false" scrolling="no"></iframe>';
 			} else {
 
 				if(settings.videoID.indexOf('FB/') > -1) {
+					// Facebook video request using the embed iframe code from their share popup
 					src = settings.videoID.split('/')[1];
-					popupIfame='<iframe class="grtyoutube-iframe" src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com' + src + '&show_text=false&width=850&t=0" width="100%" height="100%" data-autoplay="true" scrolling="no" frameborder="0" allow="clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="false"></iframe>';
+					popupIfame='<iframe class="videoPopup-iframe" src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com' + src + '&show_text=false&width=850&t=0" width="100%" height="100%" data-autoplay="true" scrolling="no" frameborder="0" allow="clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="false"></iframe>';
 				} else {
 
+					// YouTube video requested
 					// check for start and end paramters in the video id
 					var startTimeLocation = settings.videoID.indexOf('start');
 
@@ -56,16 +61,16 @@
 						src = 'https://www.youtube.com/embed/'+settings.videoID+'?rel=0&wmode=transparent&autoplay='+settings.autoPlay+'&iv_load_policy=3';
 					}
 
-					popupIfame = '<iframe class="grtyoutube-iframe" src="'+src+'" allowfullscreen frameborder="0" allow="autoplay; fullscreen"></iframe>'
+					popupIfame = '<iframe class="videoPopup-iframe" src="'+src+'" allowfullscreen frameborder="0" allow="autoplay; fullscreen"></iframe>'
 				}
 			}
 
 			// Initialize on click
 			if(getvideoid) {
 				$(this).on( "click", function() {
-					 $("body").append('<div class="grtyoutube-popup '+settings.theme+'">'+
-								'<div class="grtyoutube-popup-content">'+
-									'<span class="grtyoutube-popup-close"></span>'+
+					 $("body").append('<div class="videoPopup-popup '+settings.theme+'">'+
+								'<div class="videoPopup-popup-content">'+
+									'<span class="videoPopup-popup-close"></span>'+
 									popupIfame+
 								'</div>'+
 							'</div>');
@@ -75,14 +80,14 @@
 			// Close the box on click or escape
 			$(this).on('click', function (event) {
 				event.preventDefault();
-				$(".grtyoutube-popup-close, .grtyoutube-popup").click(function(){
-					$(".grtyoutube-popup").remove();
+				$(".videoPopup-popup-close, .videoPopup-popup").click(function(){
+					$(".videoPopup-popup").remove();
 				});
 			});
 
 			$(document).keyup(function(event) {
 				if (event.keyCode == 27){
-					$(".grtyoutube-popup").remove();
+					$(".videoPopup-popup").remove();
 				}
 			});
 		});
